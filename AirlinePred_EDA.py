5618350a1a36011c3579d229589fd6fefc2d8643
chandan brahma <cbchandan4@gmail.com>
2021-01-26 23:08:13 +0530

    Update AirlinePred_EDA.py

--- AirlinePred_EDA.py
+++ AirlinePred_EDA.py
@@ -4,7 +4,7 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
-import datetime
+import datetime 
 import seaborn as sb
 from sklearn.model_selection import train_test_split
 
